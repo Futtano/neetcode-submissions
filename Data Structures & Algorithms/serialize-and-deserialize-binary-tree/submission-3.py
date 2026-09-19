638da40b5c76f3c88045f5f43a55f2469408bc25
Daniele Loru <110403742+Futtano@users.noreply.github.com>
2026-09-19 20:05:07 +0200

    Add: serialize-and-deserialize-binary-tree - submission-3

---
+++ Data Structures & Algorithms/serialize-and-deserialize-binary-tree/submission-3.py
@@ -0,0 +1,68 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Codec:
+    # Encodes a tree to a single string.
+    # Level-order BFS approach with queue
+    # Time: O(n)
+    # Space: O(n)
+    def serialize(self, root):
+        if root is None:
+            return ''
+
+        res = []
+        queue = deque([root])
+
+        while queue:
+            node = queue.popleft()
+
+            if node is None:
+                res.append('.')
+            else:
+                res.append(str(node.val))
+                queue.append(node.left)
+                queue.append(node.right)
+
+        return ','.join(res)
+    
+        
+    # Decodes your encoded data to tree.
+    # Time: O(n)
+    # Space: O(n)
+    def deserialize(self, data):
+        if not data:
+            return None
+
+        values = data.split(',')
+
+        root = TreeNode(int(values[0]))
+        queue = deque([root])
+
+        i = 1
+
+        while queue and i < len(values):
+            node = queue.popleft()
+
+            # left child
+            if i < len(values) and values[i] != '.':
+                node.left = TreeNode(int(values[i]))
+                queue.append(node.left)
+
+            i += 1
+
+            # right child
+            if i < len(values) and values[i] != '.':
+                node.right = TreeNode(int(values[i]))
+                queue.append(node.right)
+
+            i += 1
+
+        return root
+            
+
+
+
